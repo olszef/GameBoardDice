ea0cefeffcb23c6d5059b94548545557dcfb8c2d
Mateusz Olszewski <olszef@gmail.com>
2017-05-21 12:07:32 +0200

    New static HTML pages.

---
+++ GameBoardDice/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameBoardDice.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Genre()
+        {
+            return View();
+        }
+
+        public ActionResult Cart()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        public ActionResult ShippingDetails()
+        {
+            return View();
+        }
+
+        public ActionResult ItemDetails()
+        {
+            return View();
+        }
+    }
+}
